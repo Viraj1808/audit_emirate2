9d4e17ce29b6e901079e62f154a9fd716381d066
krishna <krishnaparidwal9879@gmail.com>
2025-04-30 11:07:31 +0530

    removed dedicated page opf about

--- project/src/components/Navbar.jsx
+++ project/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   const navItems = [
     { name: 'Home', href: '/' },
     { name: 'Careers', href: '/careers' },
-    { name: 'About Us', href: '/about' },
+    
     {
       name: 'Services',
       href: '/services',
